e0a3167ac1166347351324d81241d98028428845
Juozas Gaigalas <juozasgaigalas@gmail.com>
2025-10-02 15:32:40 +0300

    first commit

---
+++ src/routes/+layout.ts
@@ -0,0 +1,17 @@
+export const ssr = false;
+export const prerender = true;
+export const trailingSlash = 'always';
+
+import '$lib/audio.svelte';
+import { loadAudioData } from '$lib/audio.svelte';
+import { loadScoreCookie } from '$lib/scoreCookie.svelte';
+// import { SplendidGrandPiano, Reverb } from "smplr";
+
+export async function load() {
+	if (typeof AudioContext !== 'undefined') {
+		loadAudioData();
+	}
+
+	loadScoreCookie();
+}
+
